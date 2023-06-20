d58bcc12277b41c4afba8e807a9d1a229debaf13
Andrzej Boba <56097001+Telchark@users.noreply.github.com>
2023-06-20 19:47:42 +0200

    asd
    .

--- src/BackendApp/OmniAPI.Domain/Models/User.cs
+++ src/BackendApp/OmniAPI.Domain/Models/User.cs
@@ -5,9 +5,10 @@ namespace OmniAPI.Domain.Models
     public class User : BaseEntity
     {
         public string Username { get; set; } = string.Empty;
-        public DateTime JoinDate { get; set; }
         [EmailAddress]
         public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public DateTime JoinDate { get; set; }
         public List<UserGame> UserGames { get; set; } = new();
         public List<UserMovie> UserMovies { get; set; } = new();
         public List<UserBook> UserBooks { get; set; } = new();
